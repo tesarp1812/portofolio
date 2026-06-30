d47926aa24c966168f2577d30f8e816df1761789
tesarp1812 <pwspratama18@gmail.com>
2026-06-30 22:43:24 +0700

    feat: Refactor JavaScript initialization and add section loading functionality
    - Moved fullpage initialization to a separate function and updated the document ready function for better organization.
    - Introduced a new script to load HTML sections dynamically on page load.
    - Added new sections for contact, experience, footer, home, portfolio, and work with relevant content.
    - Enhanced the experience section with detailed job descriptions and skills.
    - Included progress bars for skill levels in the experience section.
    - Updated the portfolio section with images and descriptions of projects.

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,15 @@
+function initFullpage() {
+    $('#fullpage').fullpage({
+        'verticalCentered': false,
+        'scrollingSpeed': 600,
+        'autoScrolling': false,
+        'css3': true,
+        'navigation': true,
+        'navigationPosition': 'right',
+    });
+}
 
-//
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
-
-// wow
-$(function()
-{
+function initWow() {
     new WOW().init();
     $(".rotate").textrotator();
-})
+}
